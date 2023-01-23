4f31020e4d5a5904b32d58b6feb2b8f729c5fad7
Santiago Stanic <119003502+theresjake@users.noreply.github.com>
2023-01-22 22:02:22 -0400

    Add files via upload
    Proyecto Java desde 0 - Java Básico

---
+++ proyecto-java-0/JavaDesdeCero.java
@@ -0,0 +1,27 @@
+package paquete;
+
+class JavaDesdeCero {
+	public static void main(String[] args) {
+	    
+	    boolean booleano = true;
+        byte 	byte1 = 48;
+        short	corto = 1000;
+        int 	entero = 4000000;
+        long 	largo = 1234567890;
+        float 	flotante = 3.9f;
+        double 	doble = 32.49d;
+        char 	caracter = 'J';
+        String  cadena = "'Este es un String'";
+
+		System.out.println("Tipos de datos:");
+		System.out.println("Boolean: " + booleano);
+		System.out.println("Byte: " + byte1);
+		System.out.println("Short: " + corto);
+		System.out.println("Integer: " + entero);
+		System.out.println("Long: " + largo);
+		System.out.println("Float " + flotante);
+		System.out.println("Double: " + doble);
+		System.out.println("Character: " + caracter);
+		System.out.println("String: " + cadena);
+	}
+}
